c50048a3d132c3cfdae49f9c1979acda9386f6a4
backdfb <wva22lfr@gmail.com>
2024-03-03 18:30:54 +0900

    페이지 삭제 후 리부트

--- src/components/StartPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function StartPage() {
-  return (
-    <h1>start</h1>
-  )
-}
-
-export default StartPage
--- src/components/contactpage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function contactpage() {
-  return (
-    <h1>contact</h1>
-  )
-}
-
-export default contactpage
