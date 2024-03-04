7cbfed7da3a3785574d66c6c326ba0be7f184f14
backdfb <wva22lfr@gmail.com>
2024-03-04 20:36:59 +0900

    상세보기 안됌

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter,
+    Routes, 
+    Route
+} from "react-router-dom";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Pages
+import MainPage from "./components/page/MainPage";
+import PostWritePage from "./components/page/PostWritePage";
+import PostViewPage from "./components/page/PostViewPage";
+
+const MainTitleText = styled.p`
+    font-size : 24px;
+    font-weight: bold;
+    text-align: center;
+`;
+
+function App(props) {
+    return (
+      <BrowserRouter>
+          <MainTitleText>개밥그릇의 BLOG ㅗㅗ</MainTitleText>
+            <Routes>
+                <Route index element={<MainPage />} />
+                <Route path="post-write" element={<PostWritePage />} />
+                <Route path="post/:postId" element={<PostViewPage />} /> 
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
